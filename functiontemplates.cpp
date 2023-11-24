1b3ffbdc4e2a9f328bbfa373867504b91bbfe48f
debbrotokumark <debkk20@gmail.com>
2023-11-24 17:15:56 +0600

    add

---
+++ functiontemplates.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+template<class anyname>
+int add (anyname a,anyname b)
+
+{
+    return a+b;
+
+}
+
+
+
+int main()
+{
+
+cout<<add(3,5);
+    return 0;
+}
+
